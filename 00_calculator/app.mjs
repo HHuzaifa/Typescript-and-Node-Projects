9fc8358f8ede9c61abcf944ef6b8418bf883585c
HHuzaifa <huzaifa50100@gmail.com>
2022-12-03 03:51:57 +0500

    .

--- 00_calculator/app.mjs
+++ 00_calculator/app.mjs
@@ -1,2 +1,29 @@
 #!/usr/bin/node
-export {};
+import chalkAnimation from "chalk-animation";
+const pausedStart = () => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, 2000);
+    });
+};
+async function displayMessage() {
+    let message = chalkAnimation.neon("Calculator Started!!!");
+    await pausedStart();
+    message.stop();
+    console.log(`
+    _______________________
+    |  _________________  |
+    | | JO           0. | |
+    | |_________________| |
+    |  ___ ___ ___   ___  |
+    | | 7 | 8 | 9 | | + | |
+    | |___|___|___| |___| |
+    | | 4 | 5 | 6 | | - | |
+    | |___|___|___| |___| |
+    | | 1 | 2 | 3 | | x | |
+    | |___|___|___| |___| |
+    | | . | 0 | = | | / | |
+    | |___|___|___| |___| |
+    |_____________________|
+    `);
+}
+displayMessage();
