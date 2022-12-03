7def374c6eb5df8a628d999a39441a95ae2fcde9
HHuzaifa <huzaifa50100@gmail.com>
2022-12-04 04:55:41 +0500

    .

--- 00_calculator/app.mjs
+++ 00_calculator/app.mjs
@@ -1,4 +1,6 @@
 #!/usr/bin/node
+import inquirer from "inquirer";
+import chalk from "chalk";
 import chalkAnimation from "chalk-animation";
 const pausedStart = () => {
     return new Promise((resolve) => {
@@ -6,10 +8,9 @@ const pausedStart = () => {
     });
 };
 async function displayMessage() {
-    let message = chalkAnimation.neon("Calculator Started!!!");
+    let message = chalkAnimation.karaoke("Calculator Started!!!");
     await pausedStart();
-    message.stop();
-    console.log(`
+    console.log(chalk.yellow(`
     _______________________
     |  _________________  |
     | | JO           0. | |
@@ -24,6 +25,56 @@ async function displayMessage() {
     | | . | 0 | = | | / | |
     | |___|___|___| |___| |
     |_____________________|
-    `);
+    `));
+    message.stop();
 }
-displayMessage();
+await displayMessage();
+async function askForNumInput() {
+    await inquirer
+        .prompt([
+        {
+            type: "list",
+            name: "Operators",
+            message: "Enter the desired operation: ",
+            choices: ["Add", "Subtract", "Multiply", "Divide"]
+        },
+        {
+            type: "number",
+            name: "Number1",
+            message: "Enter the first number: "
+        },
+        {
+            type: "number",
+            name: "Number2",
+            message: "Enter the second number: "
+        }
+    ])
+        .then((answers) => {
+        if (answers.Operators === "Add") {
+            console.log(chalk.blue(`Result of addition is: ${answers.Number1 + answers.Number2}`));
+        }
+        else if (answers.Operators === "Subtract") {
+            console.log(chalk.blue(`Result of subtraction is: ${answers.Number1 - answers.Number2}`));
+        }
+        else if (answers.Operators === "Multiply") {
+            console.log(chalk.blue(`Result of multiplication is: ${answers.Number1 * answers.Number2}`));
+        }
+        else if (answers.Operators === "Divide") {
+            console.log(chalk.blue(`Result of division is: ${answers.Number1 / answers.Number2}`));
+        }
+    });
+}
+;
+async function repeatCalcLogic() {
+    do {
+        await askForNumInput();
+        var continuity = await inquirer
+            .prompt({
+            type: "input",
+            name: "repeat",
+            message: "Press Y to continue and N to end the calculation: "
+        });
+    } while (continuity.repeat === "Y" || continuity.repeat === "y" || continuity.repeat === "yes" || continuity.repeat === "Yes" ||
+        continuity.repeat === "YES");
+}
+repeatCalcLogic();
