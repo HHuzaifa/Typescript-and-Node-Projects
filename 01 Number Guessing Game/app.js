727ac8c5add745980848078e473c343d199372f3
HHuzaifa <huzaifa50100@gmail.com>
2022-12-10 02:54:38 +0500

    .

---
+++ 01 Number Guessing Game/app.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+let book = {
+    author: {
+        firstName: "Huzaifa",
+        lastName: "Khan"
+    },
+    name: "Legacy Of Strak"
+};
+console.log(book.name, book.author.firstName);
+export {};
