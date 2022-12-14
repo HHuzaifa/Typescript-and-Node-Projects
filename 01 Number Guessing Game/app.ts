2ac724e44625665ce074dc770189caf59d86181b
HHuzaifa <huzaifa50100@gmail.com>
2022-12-14 06:30:45 +0500

    .

---
+++ 01 Number Guessing Game/app.ts
@@ -0,0 +1,49 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation"
+import { userInfo } from "os";
+
+// This is a function to display a paused welcome message
+function pausedStart() {
+    return new Promise((resolve) => {
+        setTimeout(resolve, 2000)
+    })
+}
+
+async function displayMessage() {
+    let message = chalkAnimation.karaoke("THE GAME HAS STARTED")
+    await pausedStart()
+    console.log(chalk.green(message))
+}
+
+displayMessage();
+
+async function askUser() {
+    var randomNumber: number = Math.floor(Math.random() * 5 + 1);
+    let qRepeat = await inquirer
+        .prompt([
+            {
+                type: "number",
+                name: "user_input",
+                message: "select any number from 1 to 5",
+                // validate: (answers: number) => {
+                //     if (isNaN(answers)) {
+                //         return chalk.red("Please enter a number!!!")
+                //     } else {
+                //         return true
+                //     }
+                // }
+            }
+        ])
+    console.log(qRepeat)
+    if (qRepeat.user_input === randomNumber) {
+        console.log(chalk.green("Congratulation ,your guess is right!"))
+    } else if (qRepeat.user_input < randomNumber) {
+        console.log(chalk.red(`Nope, your number ${qRepeat.userInfo} is less than guess number.`))
+    } else if (qRepeat.user_input > randomNumber) {
+        console.log(chalk.red(`Nope, your number ${qRepeat.userInfo} is higher than guess number.`))
+    };
+}
+askUser();
