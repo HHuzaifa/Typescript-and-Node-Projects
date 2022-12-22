24118f20e071bbf199b8dc7e37827faf37d82483
HHuzaifa <huzaifa50100@gmail.com>
2022-12-23 03:18:28 +0500

    .

--- 03 Todo List/app.js
+++ 03 Todo List/app.js
@@ -1,3 +1,38 @@
 #! /usr/bin/env node
-import mainScreen from "./todo modules/mainScreen.js";
-mainScreen();
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.blue("\n\n\n\t\tTODO APP\n"));
+let toDoS = [];
+let loop = true;
+while (loop) {
+    const userInput = await inquirer.prompt([
+        {
+            name: "toDo",
+            type: "input",
+            message: chalk.yellowBright("Enter the task:\n")
+        },
+        {
+            name: "addAnotherToDo",
+            type: "confirm",
+            message: chalk.yellowBright("Wanna add another task?\n"),
+            default: false
+        }
+    ]);
+    const { toDo, addAnotherToDo } = userInput;
+    loop = addAnotherToDo;
+    if (toDo) {
+        toDoS.push(toDo);
+    }
+    else {
+        console.log(chalk.red("Please enter a valid input!"));
+    }
+}
+if (toDoS.length > 0) {
+    console.log(chalk.blue("Your ToDo List Below:\n"));
+    toDoS.forEach(toDoS => {
+        console.log(chalk.green(toDoS));
+    });
+}
+else {
+    console.log(chalk.yellow("No tasks found!"));
+}
--- 03 Todo List/app.ts
+++ 03 Todo List/app.ts
@@ -1,5 +1,42 @@
 #! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
 
-import mainScreen from "./todo modules/mainScreen.js";
 
-mainScreen();
+console.log(chalk.blue("\n\n\n\t\tTODO APP\n"))
+
+
+let toDoS: string[] = [];
+let loop = true;
+
+while (loop) {
+    const userInput: { toDo: string, addAnotherToDo: boolean } = await inquirer.prompt([
+        {
+            name: "toDo",
+            type: "input",
+            message: chalk.yellowBright("Enter the task:\n")
+        },
+        {
+            name: "addAnotherToDo",
+            type: "confirm",
+            message:chalk.yellowBright("Wanna add another task?\n"),
+            default: false
+        }
+    ]);
+    const { toDo, addAnotherToDo } = userInput
+    loop = addAnotherToDo
+    if (toDo) {
+        toDoS.push(toDo)
+    } else {
+        console.log(chalk.red("Please enter a valid input!"))
+    }
+}
+
+if (toDoS.length > 0) {
+    console.log(chalk.blue("Your ToDo List Below:\n"))
+    toDoS.forEach(toDoS => {
+        console.log(chalk.green(toDoS))
+    });
+} else {
+    console.log(chalk.yellow("No tasks found!"))
+}
