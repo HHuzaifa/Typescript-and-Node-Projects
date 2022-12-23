85b102e411eacafed2aef1f76082afa65dc8d05e
HHuzaifa <huzaifa50100@gmail.com>
2022-12-24 02:34:22 +0500

    .

--- 04 Currency Converter/app.ts
+++ 04 Currency Converter/app.ts
@@ -1,2 +1,27 @@
 #! /usr/bin/env node
+import inquirer from "inquirer"
 
+
+
+
+async function getAmount1stCurrency2ndCurrency() {
+    const userInput = await inquirer.prompt([
+        {
+            name: "amount",
+            type: "number",
+            message: "Enter the amount:\n"
+        },
+        {
+            name: "1stCurrency",
+            type: "list",
+            choices: ["USD", "GBP", "EUR", "PKR", "INR", "RUB", "CNY"],
+            message: "Enter the currency you want to convert from:\n"
+        },
+        {
+            name: "2ndCurrency",
+            type: "list",
+            choices: ["USD", "GBP", "EUR", "PKR", "INR", "RUB", "CNY"],
+            message: "Enter the currency you want to convert to:\n"
+        }
+    ]);
+}
