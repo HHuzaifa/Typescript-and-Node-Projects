6714cc1bfbe9c60c6695a74bc4d2135b5887c397
HHuzaifa <huzaifa50100@gmail.com>
2023-01-02 20:01:50 +0500

    .

---
+++ 06 Student Management System/app.ts
@@ -0,0 +1,17 @@
+class person {
+    name: string
+    age: number
+    constructor(name: string, age: number) {
+        this.name = name
+        this.age = age
+    }
+    
+}
+
+class course{
+
+}
+
+class department {
+
+}
