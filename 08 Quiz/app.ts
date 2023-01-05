62b3e3d80ccbb380b400aed9202d3af13e6f47dd
HHuzaifa <huzaifa50100@gmail.com>
2023-01-06 02:59:12 +0500

    .

--- 08 Quiz/app.ts
+++ 08 Quiz/app.ts
@@ -2,13 +2,52 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
 
-const questions = [
+// An object representing quiz's question and answers.
+const questions = [] = [
     {
         question1: "What is the name of the galaxy we live in?",
-        answer: {
-            a: "Milky Way Galaxy",
-            b: "Andromeda Galaxy",
-            c: "Draco Dwarf Galaxy"
-        }
+        answer1: {
+            a: "milky Way Galaxy",
+            b: "andromeda Galaxy",
+            c: "draco Dwarf Galaxy"
+        },
+        correctAnswer: "milky Way Galaxy"
+    },
+    {
+        question2: "What is the opposite of good?",
+        answer2: {
+            a: "happy",
+            b: "bad",
+            c: "anger"
+        },
+        correctAnswer: "bad"
+    },
+    {
+        question3: "Select the vital organ of a human body.",
+        anwser3: {
+            a: "foot",
+            b: "finger",
+            c: "heart"
+        },
+        correctAnswer: "heart"
+    },
+    {
+        question4: "What is the planet's name we live on?",
+        answer4: {
+            a: "jupyter",
+            b: "earth",
+            c: "venus"
+        },
+        correctAnswer: "earth"
+    },
+    {
+        question5: "What we see in the sky at night?",
+        answer5: {
+            a: "moon",
+            b: "saturn",
+            c: "sun"
+        },
+        correctAnswer: "moon"
     }
 ]
+
