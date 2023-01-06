ccda8f50b73e4886252a8e03a60d9e42cd992d0d
HHuzaifa <huzaifa50100@gmail.com>
2023-01-07 04:58:33 +0500

    .

---
+++ 07 Adventure Game/app.js
@@ -0,0 +1,20 @@
+import chalk from "chalk";
+async function playGame() {
+    // Game variables
+    let enemies = [] = ["Skeleton", "Assassin", "Zombie"];
+    let maxEnemyHealth = 75;
+    let maxEnemyAttackDamage = 25;
+    // Player variables
+    let maxplayerHealth = 100;
+    let maxPlayerAttackDamage = 50;
+    let numOfHealthPotions = 3;
+    let healthPotionHealAmount = 30;
+    let healthPotionDropChance = 50; // Percent
+    // If True keep the game running
+    let running = Boolean;
+    console.log(chalk.red("\nWelcome to the Dungeons!!!\n"));
+    console.log(chalk.yellow.bold("-------------------------------------------------------------"));
+    let enemyHealth = Math.floor(Math.random() * enemies.length);
+    console.log(enemyHealth);
+}
+playGame();
---
+++ 07 Adventure Game/app.ts
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+
+
+async function playGame() {
+    // Game variables
+    let enemies = [] = ["Skeleton", "Assassin", "Zombie"]
+    let maxEnemyHealth = 75
+    let maxEnemyAttackDamage = 25
+
+    // Player variables
+    let maxplayerHealth = 100
+    let maxPlayerAttackDamage = 50
+    let numOfHealthPotions = 3
+    let healthPotionHealAmount = 30
+    let healthPotionDropChance = 50 // Percent
+
+    // If True keep the game running
+    let running = Boolean
+    console.log(chalk.red("\nWelcome to the Dungeons!!!\n"))
+
+    console.log(chalk.yellow.bold("-------------------------------------------------------------"))
+
+    let enemyHealth = Math.floor(Math.random() * maxEnemyHealth)
+    let enemy = enemies[Math.floor(Math.random() * enemies.length)]
+}
