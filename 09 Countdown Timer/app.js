882aea0651b712c6d4822fae198c8c7d152eb9f7
HHuzaifa <huzaifa50100@gmail.com>
2023-01-09 01:57:23 +0500

    .

--- 09 Countdown Timer/app.js
+++ 09 Countdown Timer/app.js
@@ -1,8 +1,6 @@
 #! /usr/bin/env node
 import chalk from "chalk";
 import inquirer from "inquirer";
-
-
 async function startTimer() {
     // Ask the user for the countdown duration in minutes
     const duration = await inquirer.prompt([
@@ -10,7 +8,7 @@ async function startTimer() {
             type: 'input',
             name: 'duration',
             message: chalk.blue('Enter the countdown duration in minutes:'),
-            validate: (input: string) => {
+            validate: (input) => {
                 const value = parseInt(input, 10);
                 if (!Number.isNaN(value) && value > 0) {
                     return true;
@@ -19,28 +17,22 @@ async function startTimer() {
             },
         },
     ]);
-
     // Calculate the end time by adding the duration to the current time
     const endTime = new Date().getTime() + duration.duration * 60 * 1000;
-
     // Display the countdown
     while (true) {
         // Calculate the remaining time
         const currentTime = new Date().getTime();
         const remainingTime = Math.floor((endTime - currentTime) / 1000);
-
         // If the countdown is finished, exit the loop
         if (remainingTime <= 0) {
             console.log(chalk.yellow('Countdown finished!'));
             break;
         }
-
         // Print the remaining time
         console.log(chalk.yellow(`Remaining time: ${remainingTime} seconds`));
-
         // Sleep for 1 second
         await new Promise((resolve) => setTimeout(resolve, 1000));
     }
 }
-
 startTimer();
